94ffec2fb5836490e951e1fe91488db896a14bf0
amitkumar0128 <jangidamitkumar49@gmail.com>
2025-06-25 09:31:14 +0000

    Dockerfile and docker-compose file for todo-app

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-a2ij27fg5p9u!i1zh)i_m8*l#rl92q8z7&_0^+4u+orizwd*2g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
