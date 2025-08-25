aa914db31ff5fb40718378c1c6fbe435cff4dc87
amitkumar_0128 <jangidamitkumar49@gmail.com>
2025-08-25 20:32:52 +0530

    feat: add ansible for instance configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,24 +40,6 @@ resource "aws_instance" "todo_app" {
   instance_type          = var.instance_type
   key_name               = aws_key_pair.ec2_key_pair.key_name
   security_groups = [aws_security_group.todo_sg.name]
-  
-  provisioner "remote-exec" {
-                inline = [
-                        "sudo apt-get update",
-                        "sudo apt-get install docker.io -y",
-                        "sudo apt-get install docker-compose-v2 -y",
-                        "git clone ${var.github_repo_url}",
-                        "cd ${var.github_repo_name}",
-                        "sudo docker compose up -d",
-                ]
-                connection {
-                        type        = "ssh"
-                        user        = "ubuntu"  # or "ec2-user" depending on the AMI
-                        private_key = file("${path.module}/${var.key_name}.pem")
-                        host        = self.public_ip
-                }
-
-        }
 
   tags = {
     Name    = "ToDo-EC2"
@@ -79,12 +61,11 @@ resource "aws_key_pair" "ec2_key_pair" {
 
 resource "local_file" "private_key" {
   content  = tls_private_key.ec2_key.private_key_pem
-  filename = "${path.module}/${var.key_name}.pem"
+  filename = "~/.ssh/${var.key_name}.pem"
   file_permission = "0600"
 }
 
 # Output ToDo App URL
-output "todo_app_ip" {
-  description = "ToDo App is running at http://<ip>"
-  value       = "ToDo App is running at http://${aws_instance.todo_app.public_ip}"
+output "web_server_ips" {
+  value = aws_instance.todo_app[*].public_ip
 }
